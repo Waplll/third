76bee412a09cf0d9b37d5fb79e99f0bef77d4025
Waplll <106674268+Waplll@users.noreply.github.com>
2025-02-12 20:04:47 +0700

    manipulition card

--- js/main.js
+++ js/main.js
@@ -14,30 +14,32 @@ Vue.component('note-card', {
     template: `
         <div class="card">
             <h3>{{ card.title }}</h3>
-            <ul>
-                <li v-for="(item, index) in card.items" :key="index">
-                    <input type="checkbox" v-model="item.completed" @change="updateCompletion" :disabled="column === 3">
-                    <span :class="{ 'completed': item.completed }">{{ item.text }}</span>
-                </li>
-            </ul>
-            <p v-if="card.completedDate">Завершено: {{ card.completedDate }}</p>
-            <div v-if="card.editHistory.length > 0">
-                <p>История редактирования:</p>
-                <ul>
-                    <li v-for="(date, index) in card.editHistory" :key="index">
-                        {{ date }}
-                    </li>
-                </ul>
-            </div>
-            <button v-if="column !== 3" @click="editCard">Редактировать</button>
+            <p>{{ card.description }}</p>
+            <p>Дата создания: {{ card.createdDate }}</p>
+            <p>Дэдлайн: {{ card.deadline }}</p>
+            <p v-if="card.editDates.length">Даты редактирования: {{ card.editDates.join(', ') }}</p>
+            <button @click="editCard">Редактировать</button>
+            <button @click="deleteCard">Удалить</button>
+            <button v-if="column === 1" @click="moveToInProgress">В работу</button>
+            <button v-if="column === 3" @click="moveToCompleted">Выполнено</button>
+            <button v-if="column === 2" @click="moveToTesting">На тестирование</button>
         </div>
     `,
     methods: {
-        updateCompletion() {
-            this.$emit('update-card', this.card, this.column);
-        },
         editCard() {
             this.$emit('edit-card', this.card);
+        },
+        deleteCard() {
+            this.$emit('delete-card', this.card);
+        },
+        moveToInProgress() {
+            this.$emit('move-card', this.card, 2);
+        },
+        moveToTesting() {
+            this.$emit('move-card', this.card, 3);
+        },
+        moveToCompleted() {
+            this.$emit('move-card', this.card, 4);
         }
     }
 });
@@ -47,92 +49,36 @@ let app = new Vue({
     data: {
         cards: [],
         nextCardId: 1,
-        isFirstColumnBlocked: false,
         newCardTitle: '',
-        newCardItems: ['', '', ''],
-        editingCard: null,
-    },
-    computed: {
-        firstColumnCards() {
-            return this.cards.filter(card => card.column === 1);
-        },
-        secondColumnCards() {
-            return this.cards.filter(card => card.column === 2);
-        },
-        thirdColumnCards() {
-            return this.cards.filter(card => card.column === 3);
-        },
-        isNewCardValid() {
-            return this.newCardTitle.trim() !== '' &&
-                this.newCardItems.length >= 3 &&
-                this.newCardItems.length <= 5 &&
-                this.newCardItems.every(item => item.trim() !== '');
-        }
+        newCardDescription: '',
+        newCardDeadline: '',
+        editingCard: null
     },
     methods: {
-        addCard(title, items) {
-            if (this.firstColumnCards.length >= 3) {
-                alert('Нельзя добавить больше 3 карточек в первый столбец');
-                return;
-            }
+        addCard(title, description, deadline) {
             const newCard = {
                 id: this.nextCardId++,
                 title: title,
-                items: items.map(text => ({ text: text, completed: false })),
+                description: description,
+                createdDate: new Date().toLocaleString(),
+                deadline: deadline,
                 column: 1,
-                completedDate: null,
-                editHistory: []
+                lastEdited: null,
+                editDates: [] // Массив для хранения дат редактирования
             };
             this.cards.push(newCard);
             this.saveData();
         },
         createCard() {
-            if (this.isNewCardValid) {
-                this.addCard(this.newCardTitle, this.newCardItems);
-                this.newCardTitle = '';
-                this.newCardItems = ['', '', ''];
+            if (this.newCardTitle.trim() !== '' && this.newCardDescription.trim() !== '' && this.newCardDeadline.trim() !== '') {
+                this.addCard(this.newCardTitle, this.newCardDescription, this.newCardDeadline);
+                this.resetNewCardForm();
             }
         },
-        addNewCardItem() {
-            if (this.newCardItems.length < 5) {
-                this.newCardItems.push('');
-            }
-        },
-        removeNewCardItem(index) {
-            this.newCardItems.splice(index, 1);
-        },
-        updateCard(card, column) {
-            const completedItems = card.items.filter(item => item.completed).length;
-            const totalItems = card.items.length;
-            const completionPercentage = (completedItems / totalItems) * 100;
-
-            if (column === 1) {
-                if (completionPercentage > 50) {
-                    if (this.secondColumnCards.length < 5) {
-                        card.column = 2;
-                    }
-                }
-            } else if (column === 2) {
-                if (completionPercentage <= 50) {
-                    if (this.firstColumnCards.length < 3) {
-                        card.column = 1;
-                    } else {
-                        alert('Первый столбец полон, невозможно переместить карточку обратно.');
-                    }
-                } else if (completionPercentage === 100) {
-                    card.column = 3;
-                    card.completedDate = new Date().toLocaleString();
-                }
-            }
-            this.checkSecondColumn();
-            this.saveData();
-        },
-        checkSecondColumn() {
-            if (this.secondColumnCards.length >= 5) {
-                this.isFirstColumnBlocked = true;
-            } else {
-                this.isFirstColumnBlocked = false;
-            }
+        resetNewCardForm() {
+            this.newCardTitle = '';
+            this.newCardDescription = '';
+            this.newCardDeadline = '';
         },
         editCard(card) {
             this.editingCard = JSON.parse(JSON.stringify(card));
@@ -140,7 +86,7 @@ let app = new Vue({
         saveEditedCard() {
             const index = this.cards.findIndex(c => c.id === this.editingCard.id);
             if (index !== -1) {
-                this.editingCard.editHistory.push(new Date().toLocaleString());
+                this.editingCard.editDates.push(new Date().toLocaleString()); // Добавляем дату редактирования
                 this.cards.splice(index, 1, this.editingCard);
             }
             this.editingCard = null;
@@ -149,21 +95,17 @@ let app = new Vue({
         cancelEdit() {
             this.editingCard = null;
         },
-        addEditCardItem() {
-            if (this.editingCard.items.length < 5) {
-                this.editingCard.items.push({ text: '', completed: false });
-            }
+        deleteCard(card) {
+            this.cards = this.cards.filter(c => c.id !== card.id);
+            this.saveData();
         },
-        removeEditCardItem(index) {
-            if (this.editingCard.items.length > 3) {
-                this.editingCard.items.splice(index, 1);
-            } else {
-                alert('Нельзя удалить пункт, так как должно быть не менее трех пунктов.');
+        moveCard(card, column) {
+            card.column = column;
+            if (column === 4) {
+                const now = new Date();
+                const deadline = new Date(card.deadline);
+                card.completedDate = deadline < now ? 'Просрочено' : 'Выполнено в срок';
             }
-        },
-        deleteAllCards() {
-            this.cards = [];
-            this.nextCardId = 1;
             this.saveData();
         },
         saveData() {
@@ -179,7 +121,6 @@ let app = new Vue({
             if (savedNextCardId) {
                 this.nextCardId = parseInt(savedNextCardId, 10);
             }
-            this.checkSecondColumn();
         }
     },
     mounted() {
